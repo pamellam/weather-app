6f74fb71deeef7a42c9ca620dacffd78cfe3bb83
pamellam <38889625+pamellam@users.noreply.github.com>
2024-02-22 11:51:13 +0200

    initialize search component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './components/search/Search';
+
+function App() {
+  return (
+    <>
+      <div className="container"></div>
+      <Search />
+    </>
+  );
+}
+
+export default App;
